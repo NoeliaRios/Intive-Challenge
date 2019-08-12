bf2070c507b1c1460e8ea958017cafe9371f04e5
Glasss87 <44882277+Glasss87@users.noreply.github.com>
2019-08-12 03:54:30 -0300

    base

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const urlApi = `https://randomuser.me/api/?results=50`;
 const box = document.querySelector('.box');
 
+const countryApi = `https://restcountries.eu/rest/v2/alpha/`;
+
 console.log(urlApi);
 
 fetch(urlApi)
     .then(res => res.json())
     .then(data => {
-        const primerosPerfiles = data.results.slice(0, 50);
+        const primerosPerfiles = data.results;
         console.log(primerosPerfiles);
 
-        box.innerHTML = primerosPerfiles.map(profile => `<img src="${profile.picture.thumbnail}" alt="" id=""><h1 id="">${profile.name.first}</h1><h2 id="">${profile.location.city}</h2><h3 id="">${profile.location.nat}</h3>`).join('');
+        
+
+        box.innerHTML = primerosPerfiles.map(profile => `<div id="profile_box"><div id="picture"><img src="${profile.picture.medium}"></div><h1 id="">${profile.name.first} ${profile.name.last}</h1><h2 id="">${profile.location.city}</h2><h3 id="">${profile.nat}</h3></div>`).join('');
 
 
 
