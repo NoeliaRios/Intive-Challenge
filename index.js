10b5f332f58fe8cafc03ba311c485181ea1ac787
Glasss87 <44882277+Glasss87@users.noreply.github.com>
2019-08-11 21:45:25 -0300

    photo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const urlApi = `https://randomuser.me/api/?results=10`;
+const urlApi = `https://randomuser.me/api/?results=50`;
 const box = document.querySelector('.box');
 
 console.log(urlApi);
@@ -9,7 +9,7 @@ fetch(urlApi)
         const primerosPerfiles = data.results.slice(0, 50);
         console.log(primerosPerfiles);
 
-        box.innerHTML = primerosPerfiles.map(profile => `<img src="${profile.picture.thumbnail}" alt="" id=""><h1 id=""></h1><h2 id=""></h2><h3 id=""></h3>`).join('');
+        box.innerHTML = primerosPerfiles.map(profile => `<img src="${profile.picture.thumbnail}" alt="" id=""><h1 id="">${profile.name.first}</h1><h2 id="">${profile.location.city}</h2><h3 id="">${profile.location.nat}</h3>`).join('');
 
 
 
