1c19d6839a5054e46909432f0c298e299ed83296
Glasss87 <44882277+Glasss87@users.noreply.github.com>
2019-08-13 02:57:46 -0300

    display

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const urlApi = `https://randomuser.me/api/?results=50`;
-const box = document.querySelector('.box');
-
+const box = document.querySelector('#box');
 const countryApi = `https://restcountries.eu/rest/v2/alpha/`;
 
-console.log(urlApi);
 
 fetch(urlApi)
     .then(res => res.json())
     .then(data => {
         const primerosPerfiles = data.results;
-        console.log(primerosPerfiles);
+        box.innerHTML = primerosPerfiles.map(profile => `<div id="profile_box"><div class="picture"><img src="${profile.picture.medium}"></div><h1 id="">${profile.name.first} ${profile.name.last}</h1><h2 id="">${profile.location.city}</h2><h3 id="">${profile.nat}</h3></div>`).join('');
 
-        
+        const thumbnailButton = document.getElementsByClassName('.picture');
+        const header = document.querySelector('#header');
+        console.log(thumbnailButton);
+        console.log(header);
 
-        box.innerHTML = primerosPerfiles.map(profile => `<div id="profile_box"><div id="picture"><img src="${profile.picture.medium}"></div><h1 id="">${profile.name.first} ${profile.name.last}</h1><h2 id="">${profile.location.city}</h2><h3 id="">${profile.nat}</h3></div>`).join('');
-
-
-
+        thumbnailButton.onclick = function (e) {
+            e.preventDefault();
+            header.classList.add('displayNone');
+            box.classList.add('displayNone');
+        }
     })
+
+
+
+
+
+
+    // fetch(urlApi)
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         const profileData = data.results;
+    //         box.innerHTML = profileData.map(profile => `<div id="large_profile"><div id="large_picture"><img src="${profile.picture.large}"></div><h1 id="">${profile.name.first} ${profile.name.last}</h1><h2 id="">${profile.location.city}</h2><h3 id="">${profile.nat}</h3></div>`).join('');
+
+
+    //     })
